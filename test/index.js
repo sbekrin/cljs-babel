b23764ace5e7f783fc088713da0731ccf3a28ec7
Sergey Bekrin <sergey@bekrin.me>
2017-03-08 00:35:01 -0800

    Add keyword, update grammar and parser test

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const createParser = require('../lib');
-
-const parser = createParser();
-const inputStream = fs.createReadStream('test/basic/in.cljs');
-const outputStream = fs.createWriteStream('test/basic/out.js');
-
-inputStream.pipe(parser).pipe(outputStream);
